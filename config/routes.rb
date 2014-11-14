b0dc84b37a038a1a16a61a300fa1b3ec6e70f817
LibertysYarn <code@libertysyarn.com>
2014-11-14 17:44:13 -0500

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'posts#index'
 
-  Rails.application.routes.draw do
-    resources :posts
-    root "posts#index"
+  resources :posts do
+    resources :comments
   end
+
+    root "posts#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
